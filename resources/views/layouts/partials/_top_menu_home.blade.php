afe411bb54a16f68cdd1df52f24ecee463f6daa6
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-10-26 16:19:35 +0200

    un menu + une connection

---
+++ resources/views/layouts/partials/_top_menu_home.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+            </li>        
+        
+            </li>
+        </ul>
+        <ul class="navbar-nav d-flex">
+            @if (Route::has('login'))
+                @auth
+                <li class="nav-item">
+                    <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="nav-link">Log in</a>
+                </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a href="{{ route('register') }}" class="nav-link">Register</a>
+                    </li>
+                    @endif
+                @endauth
+            @endif
+        </ul>  
+
+        </div>
+    </div>
+    </nav>
